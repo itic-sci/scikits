0901e6ab7f5093251b5e973c86c492b790afe182
xuwei <xuwei@163.com>
2023-03-28 14:00:57 +0800

    gin response.go

--- response.go
+++ response.go
@@ -6,13 +6,11 @@ import (
 )
 
 const (
-	CodeSystemErr        = -1 // 系统错误，需要修复bug
-	CodeOK               = 0  // 成功
-	CodeParamErr         = 1  // 传的参数错误，不在正常范围内
-	CodeShowErr          = 2  // 前端需要弹窗展示的错误
-	CodeApiCallLimited   = 10 // 权限错误
-	CodeLoginExpire      = 22 // 登录过期
-	CodeJumpBoundLibrary = 23 // 跳转绑定图书馆用户页面
+	CodeSystemErr       = -1 // 系统错误，需要修复bug
+	CodeOK              = 0  // 成功
+	CodeShowErr         = 2  // 前端需要弹窗展示的错误
+	CodeLoginExpired    = 10 // 登录过期
+	CodePermissionError = 20 // 没有权限
 )
 
 type ResponseFormat struct {
@@ -26,21 +24,21 @@ var codeMap = map[int]ResponseFormat{
 		Code:    CodeOK,
 		Message: "OK",
 	},
+	CodeShowErr: ResponseFormat{
+		Code:    CodeShowErr,
+		Message: "Pop hint", // 弹窗提示语
+	},
 	CodeSystemErr: ResponseFormat{
 		Code:    CodeSystemErr,
 		Message: "system error",
 	},
-	CodeParamErr: ResponseFormat{
-		Code:    CodeParamErr,
-		Message: "Param error",
-	},
-	CodeApiCallLimited: ResponseFormat{
-		Code:    CodeApiCallLimited,
-		Message: "Api Call Limited",
-	},
-	CodeLoginExpire: ResponseFormat{
-		Code:    CodeLoginExpire,
+	CodeLoginExpired: ResponseFormat{
+		Code:    CodeLoginExpired,
 		Message: "Login Expire",
+	},
+	CodePermissionError: ResponseFormat{
+		Code:    CodePermissionError,
+		Message: "Permission Limited",
 	},
 }
 
@@ -65,15 +63,11 @@ func RespondError(c *gin.Context, code int, errMsgArr ...string) {
 	c.JSON(http.StatusOK, r)
 }
 
-func RespondFormatData(c *gin.Context, data interface{}) {
+func RespondData(c *gin.Context, data interface{}) {
 	r := ResponseFormat{
 		Code:    CodeOK,
 		Message: "Ok",
 		Data:    data,
 	}
 	c.JSON(http.StatusOK, r)
-}
-
-func RespondData(c *gin.Context, data interface{}) {
-	c.JSON(http.StatusOK, data)
 }
