382d4002b6ea74045fdf0f4da5696b855d1d335b
xuwei <xuwei@163.com>
2023-04-23 18:40:07 +0800

    viper.go 可以通过 go run main.go -c xxx.yaml 文件

--- viper.go
+++ viper.go
@@ -1,6 +1,8 @@
 package scikits
 
 import (
+	"flag"
+	"fmt"
 	"github.com/spf13/viper"
 )
 
@@ -9,22 +11,32 @@ var (
 )
 
 const (
-	path          = "./"
-	fileName      = "settings"
-	fileNameLocal = "settings.local"
+	defaultFile  = "settings.local.yaml"
+	prodFileName = "settings.yaml"
 )
 
 func init() {
-	MyViper.AddConfigPath(path)
-	MyViper.SetConfigName(fileNameLocal) //设置读取的文件名
-	MyViper.SetConfigType("yaml")        //设置文件的类型
+	var config string
+	flag.StringVar(&config, "c", "", "choose config file.")
+	flag.Parse()
+
+	if config == "" { // 判断命令行参数是否为空
+		config = defaultFile
+		fmt.Printf("您正在使用默认的config路径，为%s\n", config)
+	} else { // 命令行参数不为空 将值赋值于config
+		fmt.Printf("您正在使用命令行的-c参数传递的值,config的路径为%s\n", config)
+	}
+
+	MyViper.SetConfigFile(config) //设置读取的文件名
+	MyViper.SetConfigType("yaml") //设置文件的类型
 
 	// 判断是否有settings.local，没有的话继续 settings
 	if err := MyViper.ReadInConfig(); err != nil {
-		MyViper.SetConfigName(fileName) //设置读取的文件名
+		config = prodFileName
+		fmt.Printf("您正在使用默认的config路径，为%s\n", config)
+		MyViper.SetConfigFile(config) //设置读取文件
 		if err = MyViper.ReadInConfig(); err != nil {
 			panic(err)
 		}
 	}
-
 }
